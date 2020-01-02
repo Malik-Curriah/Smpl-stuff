eba8437ce383fb19a6f56c34d43a53258a5a41b1
Malik <greendon102@gmail.com>
2020-01-02 12:42:23 -0500

    created new AST nodes for the boolean logic operators

--- src/fnplot/syntax/ExpNot.java
+++ src/fnplot/syntax/ExpNot.java
@@ -1,1 +1,27 @@
 package fnplot.syntax;
+
+import fnplot.semantics.Visitor;
+import fnplot.sys.FnPlotException;
+
+public class ExpNot extends Exp {
+
+    Exp exp;
+
+    public ExpNot(Exp e) {
+        this.exp = e;
+    }
+
+    public Exp getExp() {
+        return exp;
+    }
+
+    @Override
+    public <S, T> T visit(Visitor<S, T> v, S arg) throws FnPlotException {
+        return v.visitExpNot(this, arg);
+    }
+
+    @Override
+    public String toString() {
+        return "not" + exp1.toString();
+    }
+}
